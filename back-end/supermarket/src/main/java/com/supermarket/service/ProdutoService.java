58eddbe0a3a83673a8caaf603089eb0793905e00
thalysf <thalysfcandido@gmail.com>
2022-04-09 18:22:22 -0300

    Fix import

--- back-end/supermarket/src/main/java/com/supermarket/service/ProdutoService.java
+++ back-end/supermarket/src/main/java/com/supermarket/service/ProdutoService.java
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Set;
 
 @Service
